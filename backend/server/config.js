998f44737c99866d7441290e8ab72746fe4ff995
Michael Hueter <mthueter@gmail.com>
2018-04-26 13:25:59 -0700

    dev logging

--- backend/server/config.js
+++ backend/server/config.js
@@ -1,4 +1,4 @@
-const ENV = process.env.NODE_ENV;
+const ENV = process.env.NODE_ENV || 'development';
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/linkedlist';
 const PORT = process.env.PORT || 5000;
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'test';
